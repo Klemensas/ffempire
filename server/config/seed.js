a241cf6fd54f60abc6cc710e64bd64354dd7982b
Klemensas <kdranseika@gmail.com>
2016-03-11 23:39:17 +0200

    Create restaurants on seed and assign random restaurants to default users

--- server/config/seed.js
+++ server/config/seed.js
@@ -7,88 +7,74 @@
 // import Thing from '../api/thing/thing.model';
 import User from '../api/user/user.model';
 import Restaurant from '../api/restaurant/restaurant.model';
+// Loop through 100*100 fields generating restaurants at %
+let chance = 0.22;
+let restaurants = [];
+for (let i = 1; i < 100; i++) {
+    for (let j = 1; j < 100; j++) {
+        if (Math.random() <= chance) {
 
-// Thing.find({}).removeAsync()
-//   .then(() => {
-//     Thing.create({
-//       name: 'Development Tools',
-//       info: 'Integration with popular tools such as Bower, Grunt, Babel, Karma, ' +
-//              'Mocha, JSHint, Node Inspector, Livereload, Protractor, Jade, ' +
-//              'Stylus, Sass, and Less.'
-//     }, {
-//       name: 'Server and Client integration',
-//       info: 'Built with a powerful and fun stack: MongoDB, Express, ' +
-//              'AngularJS, and Node.'
-//     }, {
-//       name: 'Smart Build System',
-//       info: 'Build system ignores `spec` files, allowing you to keep ' +
-//              'tests alongside code. Automatic injection of scripts and ' +
-//              'styles into your index.html'
-//     }, {
-//       name: 'Modular Structure',
-//       info: 'Best practice client and server structures allow for more ' +
-//              'code reusability and maximum scalability'
-//     }, {
-//       name: 'Optimized Build',
-//       info: 'Build process packs up your templates as a single JavaScript ' +
-//              'payload, minifies your scripts/css/images, and rewrites asset ' +
-//              'names for caching.'
-//     }, {
-//       name: 'Deployment Ready',
-//       info: 'Easily deploy your app to Heroku or Openshift with the heroku ' +
-//              'and openshift subgenerators'
-//     });
-//   });
+            restaurants.push({
+                name: 'Restaurant #' + i * j,
+                location: [i, j]
+            });
+        }
+    }
+}
 
-let restaurants = [
-  {
-    name: 'Kebab Town',
-    location: [10,4]
-  }, {
-    name: 'Dux tawn',
-    location: [5,2]
-  }
-];
-
-let users = [
-  {
+let users = [{
     provider: 'local',
     name: 'Test User',
     email: 'test@test.com',
     password: 'test',
     gameData: {
-      active: true,
-      restaurants: null
+        active: true,
+        restaurants: null
     },
     role: 'admin'
-  },
-  {
+}, {
     provider: 'local',
     name: 'Admin User',
     email: 'admin@admin.com',
     password: 'test',
     gameData: {
-      active: true,
-      restaurants: null
+        active: true,
+        restaurants: null
     }
-  }
-]
+}];
 
-function populateUsers(rest, i) {
-  users[i].gameData.restaurants = rest;
-  return User.createAsync(users[i]);
+let savedRestaurants = [];
+
+function populateUsers() {
+    users.forEach((user, i) => {
+        let ind = Math.floor(Math.random() * savedRestaurants.length);
+        let rest = savedRestaurants.splice(ind, 1);
+        user.gameData.restaurants = rest;
+        User.createAsync(user)
+            .then((user) => {
+                if (i === users.length - 1) {
+                    console.log('Seeding done.');
+                }
+            });
+    });
+    return;
 }
 
+console.log('Seeding database.');
 User.find({}).removeAsync().then(() => {
-  Restaurant.find({}).removeAsync()
-    .then(() => {
-      restaurants.forEach((rest, i) => {
-        Restaurant.createAsync(rest)
-        .then((rest) => populateUsers(rest, i)); 
-      });
+    Restaurant.find({}).removeAsync()
+        .then(() => {
+            restaurants.forEach((rest, i) => {
+                Restaurant.createAsync(rest)
+                    .then((rest) => {
+                        savedRestaurants.push(rest);
+                        if (i === restaurants.length - 1) {
+                            populateUsers();
+                        }
+                        return;
+                    });
+            });
+            return true;
+        });
     return true;
-    });
-    console.log('running seed...');
-  return true;
 });
-
