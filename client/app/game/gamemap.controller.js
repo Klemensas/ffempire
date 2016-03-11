ba111bfe9df8768d5f81c7c77e5797883b7bb9ca
Klemensas <kdranseika@gmail.com>
2016-03-11 23:36:38 +0200

    Remove fabric, add seedrandom, use dom elements instead of canvas

--- client/app/game/gamemap.controller.js
+++ client/app/game/gamemap.controller.js
@@ -2,87 +2,75 @@
 
 (function() {
 
-class GameMapController {
+    class GameMapController {
 
-  constructor($http, $scope, Auth) {
-    const user = Auth.getCurrentUser();
-    this.mapFocus=  user.gameData.restaurants[0].location;
-    this.mapSize = [800, 800];
+        constructor($http, $scope, Auth) {
+            const user = Auth.getCurrentUser();
+            this.restaurants = [];
 
-    this.$http = $http;
-    this.awesomeThings = [];
+            this.mapCenter = user.gameData.restaurants[0].location;
+            this.mapSize = 800;
+            this.rowSize = 7;
+            this.yAxis = [];
+            this.xAxis = [];
+            Math.seedrandom(window.location.host);
 
-    // this.mapData = 44;
-    this.lab = 'asda';
+            $http.get('/api/restaurant/map').then(response => {
+                this.restaurants = response.data.map((el) => el.location.join());
+                this.drawMap();
+            });
 
-    // var canvasEl = document.getElementById('map-canvas');
-    let canvas = new fabric.Canvas('map-canvas');
-    this.drawMap(this.mapSize, this.mapFocus, canvas);
-    // this.canvasListen(canvasEl);
-    console.log(canvas);
-    $scope.$on('$destroy', function() {
-      // socket.unsyncUpdates('thing');
-    });
-  }
-
-// Canvas is actually 2x bigger than displayed
-// By default it has an offset of 25%
-// Map should first scroll around the offset
-// After the offset is 0% or close it should start scrolling
-  canvasListen(canvas) {
-    let offset = [-200, -200];
-    let dragging = false;
-    let original;
-    canvas.addEventListener('mousedown', function(ev) {
-      dragging = true;
-      original = [ev.offsetX, ev.offsetY];
-    });
-    canvas.addEventListener('mouseup', function(ev) {
-      dragging = false;
-    });
-    canvas.addEventListener('mousemove', function(ev) {
-      if(dragging) {
-        let drag = [original[0] - ev.offsetX, original[1] - ev.offsetY];
-        console.log(original);
-        canvas.style.transform = 'translate(' + (offset[0] - drag[0]) + 'px, ' + (offset[1] - drag[1]) + 'px)';
-        console.log((original[0] - drag[0]), (original[1] - drag[1]) + 'px)');
-        console.log('translate(' + (original[0] - drag[0]) + 'px, ' + (original[1] - drag[1]) + 'px)');
-        // canvas.
-      }
-    });
-  }
-
-  drawMap(size, focus, canvas) {
-    let rowSize = 10;
-    let fieldSize = [size[0]/rowSize, size[1]/rowSize];
-    // console.log('draw', fieldSize);
-    let images = {};
-    fabric.Image.fromURL('/assets/grass.png', function(img) {
-      images.grass = img;
-      fabric.Image.fromURL('/assets/restaurant.png', function(img) {
-        images.restaurant = img;
-        let grass = Array(rowSize*rowSize).fill(images.grass);
-          canvas.add.apply(...grass);
-          console.log(canvas);
-        for(var i = 0; i < rowSize*rowSize; i++) {
-          // images.grass.left = 200;
-          // images.grass.top = 200;
-          // ctx.fillStyle = i%3 === 0 ? 'red' : 'blue';
-          // ctx.strokeStyle = 'black';
-          // ctx.strokeRect(Math.floor(i%rowSize)*fieldSize[0], Math.floor(i/rowSize)*fieldSize[1], fieldSize[0], fieldSize[1]);
-          // ctx.fillRect(Math.floor(i%rowSize)*fieldSize[0], Math.floor(i/rowSize)*fieldSize[1], fieldSize[0], fieldSize[1]);
+            $scope.$on('$destroy', function() {
+                // socket.unsyncUpdates('thing');
+            });
         }
-      });
-    }, {
-      width: fieldSize[0],
-      height: fieldSize[1]
-    });
 
+        createField(res, field) {
+            let restaurant = res ? '<div class="restaurant"></div>' : '';
+            console.log(field);
+            return '<div data-field="' + field + '" class="map-field">' + restaurant + '</div>';
+        }
 
-  }
-}
+        drawMap() {
+            let fieldTypes = 7;
+            let start = [this.mapCenter[0] - Math.floor(this.rowSize / 2), this.mapCenter[1] - Math.floor(this.rowSize / 2)];
+            this.xAxis = _.range(start[0], start[0] + this.rowSize);
+            this.yAxis = _.range(start[1], start[1] + this.rowSize);
+            let fields = '';
+            for (let i = 0; i < this.rowSize * this.rowSize; i++) {
+                let x = start[0] + i % this.rowSize;
+                let y = start[1] + Math.floor(i / this.rowSize);
+                let pos = [x, y];
+                let res = false;
+                let field = Math.floor(Math.random() * fieldTypes + 1);
+                if (this.restaurants.indexOf(pos.join()) > -1) {
+                    res = true;
+                    field = 1;
+                }
+                fields += this.createField(res, field);
+            }
+            // Add attributes of size to container
+            let mapEl = document.getElementById('map-container');
+            mapEl.innerHTML = fields;
+            mapEl.parentNode.setAttribute('data-row-size', this.rowSize);
+            mapEl.parentNode.setAttribute('data-column-size', this.rowSize);
+            mapEl.setAttribute('class', 'active');
+        }
 
-angular.module('faster')
-  .controller('GameMapController', GameMapController);
+        mapScroll(position) {
+            let values = {
+                up: [-1, 1],
+                down: [1, 1],
+                left: [-1, 0],
+                right: [1, 0]
+            };
+            this.mapCenter[values[position][1]] += values[position][0] * this.rowSize;
+            console.log(this.mapCenter);
+            this.drawMap();
+        }
+    }
+
+    angular.module('faster')
+        .controller('GameMapController', GameMapController);
 
 })();
