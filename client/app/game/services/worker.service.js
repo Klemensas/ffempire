de6882120c867c6c063005b1ad90a32e349657d8
Klemensas <kdranseika@gmail.com>
2016-04-12 11:26:42 +0300

    Working worker service

--- client/app/game/services/worker.service.js
+++ client/app/game/services/worker.service.js
@@ -1,12 +1,53 @@
 (function () {
-  function Worker($http, Auth) {
-    const user = Auth.getCurrentUser();
+  function Worker($http, $q, Restaurant, Building) {
+    let kitchenWorkers;
+    let outsideWorkers;
+
+    function canTrain(reqs) {
+      if (!!reqs) {
+        return Building.meetsRequirements(reqs);
+      }
+      return !kitchenWorkers ? false :
+        kitchenWorkers.some(w => Building.meetsRequirements(w.requires));
+    }
+
+    function getWorkerData() {
+      if (!!kitchenWorkers) {
+        return $q.when({ kitchenWorkers, outsideWorkers });
+      }
+      return $http.get('/api/worker').then(response => {
+        kitchenWorkers = response.data.kitchenWorkers;
+        console.log(kitchenWorkers);
+        outsideWorkers = response.data.outsideWorkers;
+        return { kitchenWorkers, outsideWorkers };
+      });
+    }
+
+    function hireAttempt(worker) {
+      return $http.post('/api/worker/hireWorker', { rest: Restaurant.activeRestId, worker: worker.title })
+        .then(res => {
+          console.log('res');
+
+          Restaurant.updateRest(res.data);
+          return res.data;
+        })
+        .catch(err => {
+          console.error(err);
+          throw 'Server error';
+        });
+    }
 
     return {
-      production,
+      canTrain,
+      getWorkerData,
+      hireAttempt,
+      kitchenWorkers,
+      outsideWorkers,
+      canTrainAny: canTrain,
     };
   }
 
   angular.module('faster')
     .factory('Worker', Worker);
 }());
+
