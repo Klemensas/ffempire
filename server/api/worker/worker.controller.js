98b216af7c92a17480cf911f1c83f83582603ad7
Klemensas <kdranseika@gmail.com>
2016-05-15 17:41:14 +0300

    Add worker queues when hiring

--- server/api/worker/worker.controller.js
+++ server/api/worker/worker.controller.js
@@ -1,6 +1,8 @@
 import workers from '../../config/game/workers';
 import { Restaurant, updateRes } from '../restaurant/restaurant.model';
 import buildings from '../../config/game/buildings';
+import events from '../../components/events';
+import _ from 'lodash';
 
 // Gets worker data
 export function index(req, res) {
@@ -26,42 +28,44 @@ function handleEntityNotFound(res) {
 
 // Add worker to user restaurant
 export function hireWorkers(req, res) {
-  // TODO: hire not only kitchen workers
   const data = req.body;
-  if (data.rest && isOwner(req.user, data.rest) && typeof data.worker === 'string') {
-    Restaurant.findById(data.rest)
-      .then(handleEntityNotFound(res))
-      .then(rest => {
-        const workerTarget = workers.kitchenTitles.indexOf(data.worker) === -1 ? 'outside' : 'kitchen';
-        const target = workers[`${workerTarget}Workers`].find(w => w.title === data.worker);
-        // no worker found
-        if (!target) {
-          // TODO: error, no worker
-          return res.status(401).end();
-        }
-        // loop through resources, subtracting the costs and returning if the player affords it
-        rest = updateRes(rest);
-        const affords = buildings.resources.every(r => {
-          rest.resources[r] -= target.costs[r];
-          return rest.resources[r] >= 0;
-        });
-        if (!affords) {
-          // TODO: error, can't afford
-          res.status(401).end();
-          return;
-        }
-        const worker = rest.workers[workerTarget].find(w => w.title === target.title);
-        worker.count++;
-        Restaurant.update({ _id: rest._id, nonce: rest.nonce }, rest)
-          .then(r => {
-            if (r.nModified) {
-              return res.json(rest);
+  if (data.rest && isOwner(req.user, data.rest) && data.workers) {
+    const targets = Object.keys(data.workers);
+    const correctValues = targets.every(t => !!workers.allWorkers[t]);
+    if (correctValues) {
+      return Restaurant.findById(data.rest)
+        .then(handleEntityNotFound(res))
+          .then(rest => {
+            const costs = {
+              megabucks: 0,
+              burgers: 0,
+              fries: 0,
+              drinks: 0,
+              loyals: 0,
+            };
+            const canBuild = targets.every(t => {
+              const workerData = workers.allWorkers[t];
+              Object.keys(workerData.costs).forEach(k => costs[k] += workerData.costs[k] * data.workers[t]);
+              return Object.keys(workerData.requires).every(k => rest.buildings.find(b => b.title === k).level >= workerData.requires[k]);
+            });
+            rest = updateRes(rest);
+            const canAfford = Object.keys(costs).every(r => {
+              rest.resources[r] -= costs[r];
+              return rest.resources[r] >= 0;
+            });
+            if (canBuild && canAfford) {
+              rest = events.queueRecruits(rest, data.workers);
+              Restaurant.update({ _id: rest._id, nonce: rest.nonce }, rest)
+                .then(r => {
+                  if (r.nModified) {
+                    return res.json(rest);
+                  }
+                  return setTimeout(hireWorkers, 10, req, res);
+                });
             }
-            return setTimeout(hireWorkers, 10, req, res);
           });
-    });
-  } else {
-    console.log('error');
+    }
   }
+  return res.status(401).end();
 }
 
--- server/components/events.js
+++ server/components/events.js
@@ -1,8 +1,12 @@
 import Restaurant from '../api/restaurant/restaurant.model';
 import buildings from '../config/game/buildings';
+import workers from '../config/game/workers';
 
 function findSoonest(events) {
-  return events.building.length ? events.building[0].ends : null;
+  let soonest = 0;
+  soonest = events.building.length ? events.building[0].ends : soonest;
+  soonest = events.unit.length && events.unit[0].ends < soonest ? events.unit[0].ends : soonest;
+  return soonest;
 }
 
 function queuedBuildings(buildings) {
@@ -18,16 +22,29 @@ function checkQueueAndUpdate(rest) {
   if (rest.events.soonest > time) {
     return rest;
   }
-  const endedEvents = [];
+  // TODO: dry this mess up
+  const endedBuildings = [];
   for (let i = 0; i < rest.events.building.length; i++) {
     const item = rest.events.building[i];
-    if (item.ends <= time) {
-      endedEvents.push(item);
-      rest.events.building.splice(i, 1);
-      i--;
+    if (item.ends > time) {
+      break;
     }
+    endedBuildings.push(item);
+    rest.events.building.splice(i, 1);
+    i--;
   }
-  updateBuildings(rest, endedEvents);
+  const endedUnits = [];
+  for (let i = 0; i < rest.events.unit.length; i++) {
+    const item = rest.events.unit[i];
+    if (item.ends > time) {
+      break;
+    }
+    endedUnits.push(item);
+    rest.events.unit.splice(i, 1);
+    i--;
+  }
+  updateBuildings(rest, endedBuildings);
+  updateUnits(rest, endedUnits);
   rest.events.soonest = findSoonest(rest.events);
   return rest;
 }
@@ -39,14 +56,26 @@ function updateBuildings(rest, targets) {
   });
   return rest;
 }
+function updateUnits(rest, targets) {
+  targets.forEach(e => {
+    const unit = rest.workers.kitchen.find(w => w.title === e.target) || rest.workers.outside.find(w => w.title === e.target);
+    unit.count += (e.amount - e.produced);
+  });
+  return rest;
+}
+
+function lastItemEnds(queue) {
+  const item = queue[queue.length - 1];
+  if (!item) {
+    return 0;
+  }
+  return item.ends.getTime();
+}
 
 function queueBuilding(rest, building, level) {
   const time = Date.now();
   const buildTime = buildings.buildTimes[building.title][level] * 1000;
-  let ends = time + buildTime;
-  for (const item of rest.events.building) {
-    ends = item.ends.getTime() + buildTime;
-  }
+  const ends = (lastItemEnds(rest.events.building) || time) + buildTime;
   rest.events.building.push({
     type: 'build',
     target: building.title,
@@ -57,6 +86,25 @@ function queueBuilding(rest, building, level) {
   return rest;
 }
 
+function queueRecruits(rest, recruits) {
+  // Sort necessary if queueing multiple units and we'd like to queue in a non default order
+  const targets = Object.keys(recruits).sort((a, b) => workers.workerTypes.indexOf(a) - workers.workerTypes.indexOf(b));
+  const time = Date.now();
+  targets.forEach(t => {
+    const unit = workers.allWorkers[t];
+    const buildTime = unit.buildTime * recruits[t] * 1000;
+    const ends = (lastItemEnds(rest.events.unit) || time) + buildTime;
+    rest.events.unit.push({
+      target: t,
+      queued: time,
+      amount: recruits[t],
+      produced: 0,
+      ends,
+    });
+  });
+  rest.events.soonest = findSoonest(rest.events);
+  return rest;
+}
 
 let updatingQueue = false;
 function moveQueues() {
@@ -84,4 +132,4 @@ function moveQueues() {
 
 moveQueues();
 
-export default { queueBuilding, queuedBuildings, checkQueueAndUpdate };
+export default { queueBuilding, queuedBuildings, queueRecruits, checkQueueAndUpdate };
